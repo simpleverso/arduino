0133b97f210d2d82a2576e83f6e894f019de66be
Gonzalo santiago martinez <simpleverso@users.noreply.github.com>
2019-07-17 16:37:11 -0500

    Update nrf24l01_WifiJammer.ino

--- nrf24l01_WifiJammer.ino
+++ nrf24l01_WifiJammer.ino
@@ -1,25 +1,25 @@
 /*
-* Arduino nRF24L01 Noise Gen
-*                
+* Arduino Noise Gen
+*
 * by Hugo Rezende, www.hugorezende.com.br
 * 
 * Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
 */
-#include <SPI.h&gt;
-#include <nRF24L01.h&gt;
-#include <RF24.h&gt;
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
 RF24 radio(7, 8); // CE, CSN
- 
+
 void setup() {
   radio.begin();
   radio.setPALevel(RF24_PA_HIGH);
+  radio.setChannel(22);
   radio.stopListening();
 }
 void loop() {
-  //send it throug the channel 4 (2.2417GHz - 2437GHz)
   for(int i=16; i<38; i++){
     radio.setChannel(i);
-    const char text[] = "320939210nsdf"; //just some random string
-    radio.write(&text, sizeof(text));    //just some random string
+    const char text[] = "320939210nsdf";
+    radio.write(&text, sizeof(text));
   }
 }
